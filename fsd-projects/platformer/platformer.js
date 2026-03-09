95ced0ab865162efd85a9f7e170663682747d64d
duckareawsome <cory85885@gmail.com>
2026-03-09 13:13:00 +0000

    bouning box complete

--- fsd-projects/platformer/platformer.js
+++ fsd-projects/platformer/platformer.js
@@ -1,3 +1,4 @@
+
 $(function () {
   // initialize canvas and context when able to
   canvas = document.getElementById("canvas");
@@ -27,23 +28,27 @@ $(function () {
     //////////////////////////////////
 
     // TODO 1 - Enable the Grid
-    // toggleGrid();
+      toggleGrid()
 
 
     // TODO 2 - Create Platforms
-
-
-
-
+createPlatform(800, 300, 200, 29,"black");
+createPlatform(300, 600, 134, 00, "light blue");
+createPlatform(500, 500, 134, 00, "pink");
+createPlatform(670, 400, 134, 00, "blue")
+createPlatform(100, 700, 130, 50, "gray")
     // TODO 3 - Create Collectables
-
+createCollectable("steve", 800, 200);
+createCollectable("diamond", 200, 170, 1, 1);
+createCollectable("squirtle", 200, 300,1, 1);
 
 
     
     // TODO 4 - Create Cannons
-
-
-    
+createCannon("left", 200, 4000);
+createCannon("right", 200, 100);
+createCannon("bottom", 800, 000);
+ createCannon("right", 800, 000);   
     
     //////////////////////////////////
     // ONLY CHANGE ABOVE THIS POINT //
